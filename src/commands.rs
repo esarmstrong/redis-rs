f8ec3af413fa0f6ceacc89213b248593a6dfb2f9
Evan Armstrong <42162689+esarmstrong@users.noreply.github.com>
2020-09-16 04:03:25 +0000

    Update commands.rs

--- src/commands.rs
+++ src/commands.rs
@@ -218,7 +218,7 @@ implement_commands! {
     }
 
     /// Set json value of a key.
-    fn set_json<V: ToRedisArgs, P: ToRedisArgs>(key: String, path: P, value: V) {
+    fn set_json(key: String, path: String, value: String) {
         cmd("JSON.SET").arg(key).arg(path).arg(value)
     }
 
